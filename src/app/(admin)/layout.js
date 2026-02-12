dcb02e96caba968ebf6d4152bc9abab50833b3e3
Shahriar Ahmed Shawon <jbc.shawon60@gmail.com>
2026-02-13 00:38:23 +0600

    refactor: Reorganize admin pages into a new `(template)/(admin-template)` structure and add new dashboard, authentication, and error pages.

--- src/app/(admin)/layout.js
+++ src/app/(admin)/layout.js
@@ -1,47 +1,5 @@
-"use client";
-
-import { useSidebar } from "@/context/SidebarContext";
-import AppHeader from "@/layout/AppHeader";
-import AppSidebar from "@/layout/AppSidebar";
-import Backdrop from "@/layout/Backdrop";
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { useRouter } from "next/navigation";
-import { fetchUserProfile } from "@/redux/features/auth-slice";
-
-export default function AdminLayout({
+export default function FullWidthPageLayout({
   children
 }) {
-  const {
-    isExpanded,
-    isHovered,
-    isMobileOpen
-  } = useSidebar();
-
-  const dispatch = useDispatch();
-  const router = useRouter();
-  const { isAuthenticated, isLoading, user } = useSelector((state) => state.auth);
-
-  useEffect(() => {
-    if (!isAuthenticated && !user) {
-      dispatch(fetchUserProfile())
-        .unwrap()
-        .catch(() => {
-          router.push("/signin");
-        });
-    }
-  }, [isAuthenticated, user, dispatch, router]);
-
-  if (isLoading || !isAuthenticated) {
-    return <div className="flex items-center justify-center min-h-screen">Loading...</div>;
-  }
-  const mainContentMargin = isMobileOpen ? "ml-0" : isExpanded || isHovered ? "lg:ml-[290px]" : "lg:ml-[90px]";
-  return <div className="min-h-screen xl:flex">
-    <AppSidebar />
-    <Backdrop />
-    <div className={`flex-1 min-w-0 transition-all duration-300 ease-in-out ${mainContentMargin}`}>
-      <AppHeader />
-      <div className="p-4 mx-auto max-w-(--breakpoint-2xl) md:p-6">{children}</div>
-    </div>
-  </div>;
+  return <div>{children}</div>;
 }
