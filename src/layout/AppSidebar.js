32e52507f44a7dce6d02017f79d6e62158c7ccb0
Shahriar Ahmed Shawon <jbc.shawon60@gmail.com>
2026-03-11 01:37:06 +0600

    feat: Remove unused sidebar menu items and the 'Others' section.

--- src/layout/AppSidebar.js
+++ src/layout/AppSidebar.js
@@ -8,55 +8,7 @@ import { useSidebar } from "../context/SidebarContext";
 import { BoxCubeIcon, CalenderIcon, ChevronDownIcon, GridIcon, HorizontaLDots, ListIcon, PageIcon, PieChartIcon, PlugInIcon, TableIcon, UserCircleIcon, GroupIcon } from "../icons/index";
 import { useSelector } from "react-redux";
 
-const othersItems = [{
-  icon: <PieChartIcon />,
-  name: "Charts",
-  subItems: [{
-    name: "Line Chart",
-    path: "/line-chart",
-    pro: false
-  }, {
-    name: "Bar Chart",
-    path: "/bar-chart",
-    pro: false
-  }]
-}, {
-  icon: <BoxCubeIcon />,
-  name: "UI Elements",
-  subItems: [{
-    name: "Alerts",
-    path: "/alerts",
-    pro: false
-  }, {
-    name: "Avatar",
-    path: "/avatars",
-    pro: false
-  }, {
-    name: "Badge",
-    path: "/badge",
-    pro: false
-  }, {
-    name: "Buttons",
-    path: "/buttons",
-    pro: false
-  }, {
-    name: "Images",
-    path: "/images",
-    pro: false
-  }, {
-    name: "Videos",
-    path: "/videos",
-    pro: false
-  }]
-}, {
-  icon: <PlugInIcon />,
-  name: "Authentication",
-  subItems: [{
-    name: "Sign In",
-    path: "/signin",
-    pro: false
-  }]
-}];
+const othersItems = [];
 
 const AppSidebar = () => {
   const { user } = useSelector((state) => state.auth);
@@ -136,38 +88,6 @@ const AppSidebar = () => {
       icon: <UserCircleIcon />,
       name: "User Profile",
       path: "/profile"
-    }, {
-      icon: <CalenderIcon />,
-      name: "Calendar",
-      path: "/calendar"
-    }, {
-      name: "Forms",
-      icon: <ListIcon />,
-      subItems: [{
-        name: "Form Elements",
-        path: "/form-elements",
-        pro: false
-      }]
-    }, {
-      name: "Tables",
-      icon: <TableIcon />,
-      subItems: [{
-        name: "Basic Tables",
-        path: "/basic-tables",
-        pro: false
-      }]
-    }, {
-      name: "Pages",
-      icon: <PageIcon />,
-      subItems: [{
-        name: "Blank Page",
-        path: "/blank",
-        pro: false
-      }, {
-        name: "404 Error",
-        path: "/error-404",
-        pro: false
-      }]
     }];
   }, [user]);
 
@@ -285,12 +205,6 @@ const AppSidebar = () => {
             {renderMenuItems(navItems, "main")}
           </div>
 
-          <div className="">
-            <h2 className={`mb-4 text-xs uppercase flex leading-[20px] text-gray-400 ${!isExpanded && !isHovered ? "lg:justify-center" : "justify-start"}`}>
-              {isExpanded || isHovered || isMobileOpen ? "Others" : <HorizontaLDots />}
-            </h2>
-            {renderMenuItems(othersItems, "others")}
-          </div>
         </div>
       </nav>
     </div>
